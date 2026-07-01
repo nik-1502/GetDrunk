42553b175c5a956536cc275894bdcd0001a40c5b
Niklas Lauer <niklas.priv@gmail.com>
2026-07-01 17:52:40 +0200

    Add Busfahrer page and update Pages workflow

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <main class="home-page">
-    <header class="title-frame">
-      <h1>GetDrunk</h1>
-    </header>
-  </main>
-`
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+function renderPage() {
+  if (window.location.hash === '#busfahrer') {
+    app.innerHTML = `
+      <main class="busfahrer-page">
+        <button class="back-button" type="button">Zurück</button>
+      </main>
+    `
+
+    app.querySelector<HTMLButtonElement>('.back-button')!.addEventListener('click', () => {
+      window.location.hash = ''
+    })
+    return
+  }
+
+  app.innerHTML = `
+    <main class="home-page">
+      <header class="title-frame">
+        <h1>GetDrunk</h1>
+      </header>
+      <button class="busfahrer-button" type="button">Busfahrer</button>
+    </main>
+  `
+
+  app.querySelector<HTMLButtonElement>('.busfahrer-button')!.addEventListener('click', () => {
+    window.location.hash = 'busfahrer'
+  })
+}
+
+window.addEventListener('hashchange', renderPage)
+renderPage()
